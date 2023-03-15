627e007600c10a18f8b0b5b287eb2da077169d73
Raul Venson <raulvenson@gmail.com>
2023-03-14 21:01:57 -0300

    Documentação

--- docs/SQLQuery1.sql
+++ docs/SQLQuery1.sql
@@ -8,8 +8,6 @@ tipo char(1) not null,
 primary key(id)
 );
 
-select * from planoconta;
-
 create table transacao(
 id int identity (1,1) not null,
 date datetime not null,
@@ -20,17 +18,7 @@ primary key(id),
 foreign key(planocontaid) references planoconta(id)
 );
 
-select * from transacao;
-
-insert into planoconta (descricao, tipo)
-values ('CombustÌvel', 'D');
-insert into transacao (date, valor, historico, planocontaid)
-values ('20230215 07:00', 10.5, 'Americanas', 3);
-
-select * 
-from transacao t 
-inner join planoconta p on t.planocontaid = p.id  
-where p.tipo = 'D' and t.date between '20230101' and '20230131'
-select * from transacao where valor > 200;
-select sum(valor) from transacao;
-select sum(valor)
+-- insert into planoconta (descricao, tipo)
+-- values ('Combust√≠vel', 'D');
+-- insert into transacao (date, valor, historico, planocontaid)
+-- values ('20230215 07:00', 10.5, 'Americanas', 3);
